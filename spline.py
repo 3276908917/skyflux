6c7e80fb98486c0180406a0b51354904798e16a4
Lukas Finkbeiner @ Puyi <lfinkbeiner@berkeley.edu>
2020-03-03 12:00:52 -0800

    Installation complete

---
+++ spline.py
@@ -0,0 +1,7 @@
+import RIMEz
+
+spline_function = model_data_to_spline_beam_func('./HERA_4.9m_E-pattern_151MHz.txt', np.array([151e6]))
+
+# remember that the Jones matrix depends on \hat{r} and \nu, the frequency (of what?)
+
+
