dcfd2ce3ab89c4d37e92dd8341c0100d577cca7c
Askia the Great <lfinkbeiner@berkeley.edu>
2020-04-08 17:34:11 -0700

    Re-organized the example
    Now we should be able to run it in an appropriate environment

--- healpix_ex.py
+++ healpix_ex.py
@@ -1,28 +1,27 @@
 #nside = 16, 32, 64, 128...
-nside=128
+nside = 128
 
 theta, phi = hp.pix2ang(nside,np.arange(12 * nside * nside))
-J = spline_beam_func(150e6, alt, az)
-alt = np.pi/2 - theta
-alt = theta
 alt = np.pi - theta
 az = phi
 J = spline_beam_func(150e6, az, alt)
 hp.mollview(J[:,1,0], rot=[0, 90])
-J = spline_beam_func(150e6, alt, az)
-hp.mollview(J[:,1,0], rot=[0, 90])
+
 alt = np.pi/2 - theta
 J = spline_beam_func(150e6, alt, az)
-hp.mollview(J[:,1,0], rot=[0, 90])
-hp.mollview(J[:,0,0], rot=[0, 90])
-hp.mollview(J[:,1,1], rot=[0, 90])
-hp.mollview(J[:,0,0], rot=[0, 90])
-hp.orthview(J[:,0,0], rot=[0, 90])
-hp.orthview(J[:,0,0], rot=[0, 90], half_sky=True)
-hp.orthview(np.log10(J[:,0,0]), rot=[0, 90], half_sky=True)
-J[:,0,0]
-hp.orthview(np.abs(J[:,0,0]), rot=[0, 90], half_sky=True)
-hp.orthview(np.abs(J[:,1,0]), rot=[0, 90], half_sky=True)
-hp.orthview(np.abs(J[:,0,1]), rot=[0, 90], half_sky=True)
+
+hp.mollview(J[:, 1, 0], rot=[0, 90])
+hp.mollview(J[:, 0, 1], rot=[0, 90])
+hp.mollview(J[:, 1, 1], rot=[0, 90])
+hp.mollview(J[:, 0, 0], rot=[0, 90])
+
+hp.orthview(J[:, 0, 0], rot=[0, 90])
+hp.orthview(J[:, 0, 0], rot=[0, 90], half_sky=True)
+hp.orthview(np.log10(J[:, 0, 0]), rot=[0, 90], half_sky=True)
+
+hp.orthview(np.abs(J[:, 0, 0]), rot=[0, 90], half_sky=True)
+hp.orthview(np.abs(J[:, 1, 0]), rot=[0, 90], half_sky=True)
+hp.orthview(np.abs(J[:, 0, 1]), rot=[0, 90], half_sky=True)
+hp.orthview(np.abs(J[:, 1, 1]), rot=[0, 90], half_sky=True)
 
 #https://healpix.sourceforge.io/
