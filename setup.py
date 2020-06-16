ed8975a2575da408535a62b821789b662b9ecf13
Lukas Finkbeiner @ Askia the Great <lfinkbeiner@berkeley.edu>
2020-06-16 15:23:12 -0700

    Torching some redundant scripts
    The next step will be to demonstrate that some progress on the histograms notebook

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup, find_packages
 
-# I do not have a readme yet
-#with open("README.md", "r") as fh:
-#    long_description = fh.read()
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='flux',
     version='1!1.1a1',
     author='Lukas Finkbeiner, C. D. Nunhokee, Aaron Parsons',
     author_email='lfinkbeiner@berkeley.edu',
+    description='Primitive source handling functions',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/3276908917/HERA',
     packages=find_packages(),
     package_data={'': ['gleam_with_alpha.txt', 'ant_dict.pk', 'J.npy']},
