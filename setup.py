26e7eac206c968b656866b2d10c559555aba325a
Lukas Finkbeiner @ Askia the Great <lfinkbeiner@berkeley.edu>
2020-06-16 14:24:27 -0700

    Basic universal import works
    However, we cannot find the data files. Back to setuptools we go

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+# I do not have a readme yet
+#with open("README.md", "r") as fh:
+#    long_description = fh.read()
+
+setup(
+    name='flux',
+    version='-9',
+    author='Lukas Finkbeiner, C. D. Nunhokee, Aaron Parsons',
+    author_email='lfinkbeiner@berkeley.edu',
+    url='https://github.com/3276908917/HERA',
+    packages=['flux']
+)
