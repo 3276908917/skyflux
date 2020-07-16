Fixed minor bugs with conflicting rotation function headers
This is not a stable commit: the J formatting routine is still in progress.

J_test.py
@@ -28,7 +28,7 @@ def visibility(J, ant1, ant2, source, ra, dec, nu=151e6, time=None):
     I = source.flux_by_frq[nu / 1e6]
     s = np.array([complex(I), 0, 0, 0])
 
-    r = rot.raddec2lm(ra, dec, ra0=time)
+    r = rot.radec2lm(ra, dec, ra0=time)
 
     phi = ant.phase_factor(ant1, ant2, r, nu)
 

flux/rot.py
@@ -34,7 +34,7 @@ def collapse_hour(hour, minute=0, second=0, radians=False):
 hera_lat = -collapse_angle(30, 43, 17)
 hera_lon = collapse_angle(21, 25, 42)
 
-def get_lst(hera_lon, radians=False):
+def get_lst(lon, radians=False):
     """
     Return current local sidereal time (LST)
     for longitude
@@ -261,7 +261,7 @@ written by C. D. Nunhokee,
 https://github.com/Chuneeta/polarizedSims/blob/master/genVisibility.py
 """
 # Unfortunately, the parameter order has been switched wrt the citation.
-def raddec2lm(ra, dec, ra0=None, dec0=np.radians(hera_lat)):
+def radec2lm(ra, dec, ra0=None, dec0=np.radians(hera_lat)):
     """
     Converts equatorial coordinates to direction cosines l & m
     ra   : right ascension in radians; type: float
@@ -273,7 +273,7 @@ def raddec2lm(ra, dec, ra0=None, dec0=np.radians(hera_lat)):
     """
     # See note at the end about default arguments.
     if ra0 is None:
-        ra0 = get_lst(radians=True)
+        ra0 = get_lst(np.radians(hera_lon), radians=True)
 
     l = np.cos(dec) * np.sin(ra0 - ra)
     m = -1 * (np.sin(dec) * np.cos(dec0) - \

flux/vis.py
@@ -26,7 +26,7 @@ def visibility(ant1, ant2, source, nu=151e6, time=None):
 
     ra = np.radians(source.ra_angle)
     dec = np.radians(source.dec_angle)
-    r = rot.raddec2lm(ra, dec, ra0=time)
+    r = rot.radec2lm(ra, dec, ra0=time)
 
     phi = ant.phase_factor(ant1, ant2, r, nu)
 