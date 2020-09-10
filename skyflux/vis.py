fd5b1e5b2b7f36171c3e8dd8505cc844eeba52ab
Lukas Finkbeiner @ Otto von Bismarck <lfinkbeiner@berkeley.edu>
2020-09-09 18:10:04 -0700

    Potentially controversial merger
    I have brought back default arguments to vis.py, and I have merged sources_over_time and sky_over_time into one function which handles a list of sources.
    
    In the next commit, I will automatically handle single-source direct inputs. That should make it easier on the user.

--- skyflux/vis.py
+++ skyflux/vis.py
@@ -35,26 +35,14 @@ def visibility(ant1, ant2, source, nu=151e6, time=None):
     # Hack to get rid of extra array shell surrounding answer
     return malformed_result[:, 0]
 
-def visibility_integrand(ant1, ant2, nu=151e6, time=None):
-    """
-    Return visibility integrand evaluated for all sources
-    in catalog's parsed GLEAM array, obj_catalog.
-    @ant1 and @ant2 are indices of antannae,
-        to specify a baseline
-    @nu frequency in Hertz
-    @time : local sidereal time [float, radians]
-        default: None corresponds to run-time LST.
-    """
-    total = np.array([0j, 0j, 0j, 0j]) # 4 x 1, complex encodes phase.
-    for source in catalog.obj_catalog:
-        total += visibility(ant1, ant2, source, nu, time)
-    return total
 
-def sky_over_time(ant1, ant2, start, end, interval, nu=151e6):
+def sources_over_time(ant1, ant2, list_sources=None,
+                        start=0, end=2/3*np.pi, interval=np.pi/72, nu=151e6):
     """
-    Return an array containing multiple visibility_integrand results.
-    @ant1 and @ant2 are indices of antannae,
-        to specify a baseline
+    Return an array containing the visibilities at different points of time.
+    @ant1 and @ant2 are indices of antennae, to specify a baseline
+    @list_sources is an array of GLEAM catalog objects (see catalog.py for specifications)
+        default value translates to the entire downloaded segment of the catalog
     @start: starting LST of integration [float, radians]
     @end: terminal LST of integration [float, radians]
     @interval: integration window width [float, radians]
@@ -65,42 +53,18 @@ def sky_over_time(ant1, ant2, start, end, interval, nu=151e6):
     (ant1, ant2, 0, 2 / 3 * np.pi, np.pi / 72, nu)
     to-do: error-checking on inputs
     """
-    list_visibilities = []
-    lst = start
-    while lst <= end:
-        list_visibilities.append(
-            np.array([lst, visibility_integrand(ant1, ant2, nu, lst)])
-        )
-        lst += interval
-    # perhaps not necessary. Better safe than sorry:
-    return np.array(list_visibilities)
-
-#! Maybe we can merge this function and the previous one somehow,
-    # we are re-using virtually all of the code
-# chiefly for debugging and discrete analysis purposes
-def source_over_time(ant1, ant2, source, start, end, interval, nu=151e6):
-    """
-    Return an array containing multiple visibility results.
-    @ant1 and @ant2 are indices of antannae,
-        to specify a baseline
-    @source is a GLEAM catalog object
-        (see catalog.py for specifications)
-    @start: starting LST of integration [float, radians]
-    @end: terminal LST of integration [float, radians]
-    @interval: integration window width [float, radians]
-    @nu frequency in Hertz
+    if list_sources is None
+        # make a copy to ensure write safety
+        list_sources = catalog.obj_catalog.copy()
     
-    The cold patch has start = 0 hours, end = 8 hours,
-        and we set out to measure at ten minute intervals.
-    (ant1, ant2, 0, 2 / 3 * np.pi, np.pi / 72, nu)
-    to-do: error-checking on inputs
-    """
     list_visibilities = []
     lst = start
     while lst <= end:
-        list_visibilities.append(
-            np.array([lst, visibility(ant1, ant2, source, nu, time=lst)])
-        )
+        next_vista = np.array([0j, 0j, 0j, 0j])
+        for source in list_sources:
+            next_vista += visibility(ant1, ant2, source, nu, time)
+
+        list_visibilities.append(next_vista)
         lst += interval
     # perhaps not necessary. Better safe than sorry:
     return np.array(list_visibilities)
