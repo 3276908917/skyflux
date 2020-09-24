9a2b579d5954b9c72bdd47c7ba9ed7d8f76a38b6
Lukas Finkbeiner @ Otto von Bismarck <lfinkbeiner@berkeley.edu>
2020-09-23 21:46:31 -0700

    First pass, glob pattern device

--- skyflux/generate_model.py
+++ skyflux/generate_model.py
@@ -1,18 +1,37 @@
+frqs = []
+
 def generate_model():
-    import os
+    global frqs
+
+    import glob, os
     from RIMEz import beam_models
     from spin1_beam_model import cst_processing
 
-    data_prefix = os.path.dirname(os.path.abspath(__file__)) + "/"
-    beam_origin = data_prefix + "HERA_4.9m_E-pattern_151MHz.txt"
+    data_prefix = os.path.dirname(os.path.abspath(__file__)) + \
+                  "HERA_4.9m_E-pattern_ "
+    data_suffix = "MHz.txt"
+    common_unit = 1e6 # one MHz
+
+    # generated based on magic strings above
+
+    label_start = len(data_prefix)
+
+    pattern_list = []
+    
+    for file in glob.glob("./patterns/*"):
+        label_end = len(file) - len(data_suffix)
+        label = file[label_start:label_end]
+        frq = float(label) * common_unit
+        frqs.append(frq)
+
+        pattern_list.append(file)
+    
     model_name ="ant.h5"
 
     processor = cst_processing.CSTDataProcessor(
-        [beam_origin, beam_origin, beam_origin],
-        # hard coding to demonstrate functionality elsewhere.
-        # We will fix it eventually.
-        np.array([150e6, 151e6, 152e6]),
-        1, 1e-4
+        pattern_list,
+        np.array(frqs),
+        1, 1e-4 # magic...
     )
 
     processor.compute_spin1_harmonics()
