69f8a2655b95bd081caa4ed04cc1fd6f7306d9e7
Lukas Finkbeiner @ Askia the Great <lfinkbeiner@berkeley.edu>
2020-09-28 16:34:23 -0700

    Expanded the glob parser

--- generate_model.py
+++ generate_model.py
@@ -4,22 +4,30 @@ from spin1_beam_model import cst_processing
 
 frqs = []
 
-data_prefix = os.path.dirname(os.path.abspath(__file__)) + \
-              "HERA_4.9m_E-pattern_ "
-data_suffix = "MHz.txt"
+data_dir = os.path.dirname(os.path.abspath(__file__)) + \
+           "/skyflux/patterns/"
+print("data_dir is", data_dir)
+
+# Some files have a space after the last underscore in what I guess is
+# a representation of a zero in the hundreds place. Fortunately, this
+# implementation correctly ignores white space in casting the string.
+file_prefix = "HERA_4.9m_E-pattern_"
+file_suffix = "MHz.txt"
 common_unit = 1e6 # one MHz
 
 # generated based on magic strings above
 
-label_start = len(data_prefix)
+label_start = len(data_dir + file_prefix)
 
 pattern_list = []
 
-for file in glob.glob("./skyflux/patterns/*"):
-    label_end = len(file) - len(data_suffix)
+for file in glob.glob(data_dir + "*"):
+    print("File is:", file)
+    label_end = len(file) - len(file_suffix)
     label = file[label_start:label_end]
     frq = float(label) * common_unit
     frqs.append(frq)
+    print("Frq is:", frq, "\n")
 
     pattern_list.append(file)
 
